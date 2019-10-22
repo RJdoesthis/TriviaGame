b46dbffa18932f43e865f2aae970fefb510ce129
Rodney Julien <RodneyJulien@gmail.com>
2019-10-22 11:33:46 -0400

    updated javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    //questions for the game
+    const myQuestions = [
+        {
+            question: "Who is the first President?",
+            answers: {
+                a: "Superman",
+                b: "Rodney Julien",
+                c: "George Washington"
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "Who is the best bootcamp?",
+            answers: {
+                a: "Trilogy",
+                b: "Some random guy",
+                c: "Jim"
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "Where is IKEA?",
+            answers: {
+                a: "Boston",
+                b: "Stoughton",
+                c: "Woburn",
+            },
+            correctAnswer: "b"
+        }
+    ];
+
+    const timer;
+    let correctAnswer = 0;
+    let incorrectAnswer = 0;
+    let counter = 60;
+
+
+
+});
+
+
+
